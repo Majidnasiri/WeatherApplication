b120e4ee6867d81cd28243a7d388dd9bf77c784b
Majid Nasirinejad <majid.nasirinejad@gmail.com>
2019-05-01 22:43:15 -0300

    Updates

---
+++ precache-manifest.328a82bd38df0a4d54c7e13270839728.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/WeatherApplication/static/media/Clouds_.ad789581.svg"
+  },
+  {
+    "revision": "b0939b0de07dba117682",
+    "url": "/WeatherApplication/static/css/main.3c347a83.chunk.css"
+  },
+  {
+    "revision": "266583c4c5bbef1ddfca",
+    "url": "/WeatherApplication/static/js/runtime~main.be3088c5.js"
+  },
+  {
+    "revision": "56c46d80c13ec13e8d5062a8d53620b7",
+    "url": "/WeatherApplication/static/media/_.56c46d80.svg"
+  },
+  {
+    "revision": "20c0ad3fd85b45167211",
+    "url": "/WeatherApplication/static/js/2.9dade313.chunk.js"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/WeatherApplication/static/media/Clear_.7f93fbdb.svg"
+  },
+  {
+    "revision": "b0939b0de07dba117682",
+    "url": "/WeatherApplication/static/js/main.8f58da45.chunk.js"
+  },
+  {
+    "revision": "f134a51c3c0a49f33fecc7a201a4e075",
+    "url": "/WeatherApplication/static/media/Rain_.f134a51c.svg"
+  },
+  {
+    "revision": "f78cba60ad28e30ea060d424f114670a",
+    "url": "/WeatherApplication/static/media/Snow_.f78cba60.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/WeatherApplication/static/media/Thunder_.7142c78b.svg"
+  },
+  {
+    "revision": "20c0ad3fd85b45167211",
+    "url": "/WeatherApplication/static/css/2.8e5dd05b.chunk.css"
+  },
+  {
+    "revision": "0d932c8a58beaf3709dccec8195277d4",
+    "url": "/WeatherApplication/index.html"
+  }
+];
